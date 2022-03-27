ec2d387cd065d419592d47ab2743eb11dde8815c
Zach <zachriane01@gmail.com>
2022-03-27 22:26:37 +0800

    Implemented file handling, and header files

---
+++ hospHeader.h
@@ -0,0 +1,28 @@
+#ifndef HOSPHEADER_H
+#define HOSPHEADER_H
+
+// Prescription Structure
+
+typedef struct node{
+
+    char name[30];
+    int age;
+    char medName[25];
+    int doseAmt;
+    int prioNum;
+    struct node *next;
+    struct node *prev;
+} NODE, *LL;
+
+// Function Prototypes
+
+void showMenu();
+NODE *createNode();
+void assign(NODE **new_node, char patName[], int patAge, char med[], int dose, int prio);
+void enQueue(NODE *new_node, LL *head, LL *tail);
+void deQueue(LL *head, LL *tail, int prio);
+void adjustPrio(LL *head, int prio);
+void sortList(LL *head);
+void showList(LL head);
+
+#endif
